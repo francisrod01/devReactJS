cd7cdadd14d230715a94c7ba70edadc2dc203d6d
Francis Rodrigues <dev@tosystems.net>
2017-11-11 19:07:11 -0200

    ENH: Created an empty component

---
+++ lessons/01 - First steps/EmptyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class DevPleno extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<div><p>Olá DevPleno!</p></div>)
+    }
+}
