34d6274e46df40624b1e5d65b6f85f111986e88e
Francis Rodrigues <dev@tosystems.net>
2017-11-14 20:48:20 -0200

    Added comments object data to constructor to Comments project

--- projects/comments-app/src/App.js
+++ projects/comments-app/src/App.js
@@ -5,6 +5,20 @@ import NewComment from './NewComment'
 import Comments from './Comments'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      comments: {
+        '1': {
+          comment: 'First comment'
+        },
+        '2': {
+          comment: 'Second comment'
+        }
+      }
+    }
+  }
   render() {
     return (
       <div className="container">
