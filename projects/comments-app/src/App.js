9d4aeaa054c1cf1cd7d423117857b0c1e501da65
Francis Rodrigues <dev@tosystems.net>
2017-11-14 20:41:55 -0200

    Added Comments component to Comments project

--- projects/comments-app/src/App.js
+++ projects/comments-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import 'bootstrap-css-only'
 
 import NewComment from './NewComment'
+import Comments from './Comments'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <NewComment />
+        <Comments />
       </div>
     )
   }
---
+++ projects/comments-app/src/Comments.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Comments extends Component {
+    render() {
+        return (
+            <div>
+                Rendering comments here.
+            </div>
+        )
+    }
+}
+
+export default Comments
