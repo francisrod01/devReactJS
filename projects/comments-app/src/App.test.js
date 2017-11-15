5f4d21be916f43f6991f1ce12f5c0ebc7ef8230f
Francis Rodrigues <dev@tosystems.net>
2017-11-15 14:34:07 -0200

    Testing postNewComment method and injecting dependencies Here we use jest.fn() to mock syncState with firebase We passed the base parameter to App component as well

--- projects/comments-app/src/App.test.js
+++ projects/comments-app/src/App.test.js
@@ -3,22 +3,36 @@ import {shallow, mount, render} from 'enzyme'
 
 import App from './App'
 
+
 describe('<App />', () => {
+
+  const base = {
+    syncState: jest.fn()
+  }
+
   it('renders without crashing', () => {
-    const wrapper = shallow(<App />)
+    const wrapper = shallow(<App base={base} />)
     expect(wrapper.length).toBe(1)
   })
   it('should have .container class', () => {
-    const wrapper = shallow(<App />)
+    const wrapper = shallow(<App base={base} />)
     expect(wrapper.is('.container')).toBe(true)
   })
   it('shows comments', () => {
-    const wrapper = shallow(<App />)
+    const wrapper = shallow(<App base={base} />)
     expect(wrapper.find('Comments').length).toBe(1)
   })
   it('shows NewComment', () => {
-    const wrapper = shallow(<App />)
+    const wrapper = shallow(<App base={base} />)
     expect(wrapper.find('NewComment').length).toBe(1)
+  })
+  it('adds a new comment to state when postNewComment is called', () => {
+    const wrapper = mount(<App base={base} />)
+    wrapper.instance().postNewComment({comment: 'comment test 01'})
+    wrapper.instance().postNewComment({comment: 'comment test 02'})
+    wrapper.instance().postNewComment({comment: 'comment test 03'})
+    const comments = Object.keys(wrapper.instance().state.comments)
+    expect(comments.length).toBe(3)
   })
   /*it('outputs the <App />', () => {
     const wrapperShallow = shallow(<App />)
