5ad76180f3337642876306739c774a49cbd7842e
Francis Rodrigues <dev@tosystems.net>
2017-11-15 14:11:22 -0200

    Beggining tests with App component

--- projects/comments-app/src/App.test.js
+++ projects/comments-app/src/App.test.js
@@ -8,4 +8,25 @@ describe('<App />', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.length).toBe(1)
   })
+  it('should have .container class', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.is('.container')).toBe(true)
+  })
+  it('shows comments', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Comments').length).toBe(1)
+  })
+  it('shows NewComment', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('NewComment').length).toBe(1)
+  })
+  /*it('outputs the <App />', () => {
+    const wrapperShallow = shallow(<App />)
+    const wrapperMount = mount(<App />)
+    const wrapperRender = render(<App />)
+
+    // console.log(wrapperShallow.debug())
+    // console.log(wrapperMount.debug())
+    // console.log(wrapperRender.html())
+  })*/
 })
