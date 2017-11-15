57987d5cde612f489ff804656cecbbb5a40b8a33
Francis Rodrigues <dev@tosystems.net>
2017-11-15 00:42:04 -0200

    Added a firebase config file

---
+++ projects/comments-app/src/base.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base'
+
+
+const config = {
+    apiKey: "AIzaSyA5P_bMaU-Jh1-DSwDtdp4mTnmSGSdDZiI",
+    authDomain: "devreactjs-8cdf4.firebaseapp.com",
+    databaseURL: "https://devreactjs-8cdf4.firebaseio.com",
+    projectId: "devreactjs-8cdf4",
+    storageBucket: "",
+    messagingSenderId: "514612843673"
+}
+
+const base = Rebase.createClass(config)
+
+export default base
