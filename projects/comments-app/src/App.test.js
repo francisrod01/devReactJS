c807399e14f8144baa32016a662d40acd906e9b2
Francis Rodrigues <dev@tosystems.net>
2017-11-15 12:23:42 -0200

    Added our first test

--- projects/comments-app/src/App.test.js
+++ projects/comments-app/src/App.test.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {shallow, mount, render} from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import App from './App'
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.length).toBe(1)
+  })
+})
