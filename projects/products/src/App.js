534163e6c2174ce5ad950ed1ccee05bea21e4810
Francis Rodrigues <dev@tosystems.net>
2017-11-16 00:18:53 -0200

    Created Products Management project

---
+++ projects/products/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button className='btn btn-info'>test</button>
+      </div>
+    );
+  }
+}
+
+export default App
