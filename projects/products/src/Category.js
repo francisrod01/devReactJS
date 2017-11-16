9b29b2a9eb72b2e3585087d6c4ddab0456b3d280
Francis Rodrigues <dev@tosystems.net>
2017-11-16 18:07:59 -0200

    [ProductsMgmt] Loading Category's data from json-server

--- projects/products/src/Category.js
+++ projects/products/src/Category.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class Category extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            products: [],
+            category: {}
+        }
+
+        this.loadData = this.loadData.bind(this)
+    }
+    loadData(catId) {
+        const prodUrl = `http://localhost:3001/products?category=${catId}`
+        const catUrl = `http://localhost:3001/categories/${catId}`
+        axios
+            .get(prodUrl)
+            .then(res => {
+                this.setState({
+                    products: res.data
+                })
+            })
+        
+        axios
+            .get(catUrl)
+            .then(res => {
+                this.setState({
+                    category: res.data
+                })
+            })
+    }
+    componentDidMount() {
+        const catId = this.props.match.params.catId
+        this.loadData(catId)
+    }
+    componentWillReceiveProps(newProps) {
+        const catId = newProps.match.params.catId
+        this.loadData(catId)
+    }
+    renderProduct(product, _key) {
+        return (
+            <p className='well' key={_key}>{product.product}</p>
+        )
+    }
     render() {
         return (
             <div>
-                <h1>Category {this.props.match.params.catId}</h1>
+                <h1>Category: "{this.state.category.category}"</h1>
+                { this.state.products.map(this.renderProduct) }
             </div>
         )
     }
