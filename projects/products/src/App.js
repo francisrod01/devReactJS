9fc74d7ed71f5a95724fcec03dd708e9a6a74764
Francis Rodrigues <dev@tosystems.net>
2017-11-16 01:06:15 -0200

    Added our first routes to App.js in Products project

--- projects/products/src/App.js
+++ projects/products/src/App.js
@@ -1,39 +1,46 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-          <div className='container'>
-            <a href='/' className='navbar-brand'>
-              Products Management
-            </a>
-            <button className="navbar-toggler" type="button" 
-              data-toggle="collapse" data-target="#navbarNav" 
-              aria-controls="navbarNav" aria-expanded="false" 
-              aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className='collapse navbar-collapse' id='navbarNav'>
-              <ul className='navbar-nav'>
-                <li className='nav-item'>
-                  <a className='nav-link' href='/'>Home</a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href='/'>Products</a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href='/'>About</a>
-                </li>
-              </ul>
+      <Router>
+        <div>
+          <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <div className='container'>
+              <Link to='/' className='navbar-brand'>
+                Products Management
+              </Link>
+              <button className="navbar-toggler" type="button" 
+                data-toggle="collapse" data-target="#navbarNav" 
+                aria-controls="navbarNav" aria-expanded="false" 
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav'>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/'>Home</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/products'>Products</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/about'>About</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </nav>
+          <div className='container' style={{'paddingTop': 20}}>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
           </div>
-        </nav>
-        <div className='container' style={{'paddingTop': 20}}>
-          <h1>Products Management</h1>
         </div>
-      </div>
+      </Router>
     );
   }
 }
