c91494d1a41c75bcd20fe328e07edd78d7fbc1ff
Francis Rodrigues <dev@tosystems.net>
2017-12-07 19:57:57 -0200

    [react-redux] Refactored Counter component

--- projects/redux-counter/src/Counter.js
+++ projects/redux-counter/src/Counter.js
@@ -3,16 +3,14 @@ import { connect } from 'react-redux'
 
 import { increment, decrement } from './actions'
 
-class Counter extends Component {
-    render() {
-        return (
-            <p>
-                Counter: { this.props.count } {' '}
-                <button onClick={ this.props.increment }>+</button>
-                <button onClick={ this.props.decrement }>-</button>
-            </p>
-        )
-    }
+const Counter = ({ count, increment, decrement }) => {
+    return (
+        <p>
+            Counter: { count }{' '}
+            <button onClick={ increment }>+</button>
+            <button onClick={ decrement }>-</button>
+        </p>
+    )
 }
 
 const mapStateToProps = (state) => {
