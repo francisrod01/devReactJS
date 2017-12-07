e4e67871ad4f2770d16a3682bc373719574b572b
Francis Rodrigues <dev@tosystems.net>
2017-12-07 21:44:35 -0200

    [react-redux] Using Tests to action creators

---
+++ projects/redux-counter/src/actions.test.js
@@ -0,0 +1,18 @@
+import { increment, decrement } from './actions'
+
+
+test('Action: increment', () => {
+    let incr = increment(10)
+    expect(incr).toEqual({
+        type: 'INCREMENT',
+        value: 10
+    })
+})
+
+test('Action: decrement', () => {
+    let decr = decrement(20)
+    expect(decr).toEqual({
+        type: 'DECREMENT',
+        value: 20
+    })
+})
