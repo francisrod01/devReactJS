7c863d908d114283d4f56e64fb1f01eee9c65627
Francis Rodrigues <dev@tosystems.net>
2017-12-07 19:49:10 -0200

    Created counter project by react-redux

---
+++ projects/redux-counter/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import counterReducer from './reducer'
+import Counter from './Counter'
+
+let store = createStore(counterReducer)
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Counter />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App
