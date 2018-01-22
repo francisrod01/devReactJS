a7c3bac87850b0769c2320de8bbc5da3b95771a6
Francis Rodrigues <francis.rod01@outlook.com>
2018-01-22 18:02:41 -0200

    [react-redux-thunk] Created clean files to using redux

--- projects/redux-thunk/src/App.js
+++ projects/redux-thunk/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+import { reducer } from './reducer'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          
+        </div>
+      </Provider>
     )
   }
 }
