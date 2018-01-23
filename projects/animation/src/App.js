abee3966c77818bd6bc639a60a4bc88fec80239b
Francis Rodrigues <francis.rod01@outlook.com>
2018-01-23 06:27:51 -0200

    [react-animation] Implementing react animation

--- projects/animation/src/App.js
+++ projects/animation/src/App.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactCssTransitionGroup from 'react-addons-css-transition-group'
+
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [1,2,3]
+    }
+
+    // Bindables.
+    this.remove = this.remove.bind(this)
+  }
+  remove(index) {
+    let newItems = [...this.state.items]
+    newItems.splice(index, 1)
+    this.setState({ items: newItems })
+  }
   render() {
     return (
       <div className="App">
@@ -13,9 +30,26 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ReactCssTransitionGroup
+          transitionName='anim'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
+          { this.state.items.map((value, index) => 
+            (
+              <div key={value}>
+                {value}
+                <button onClick={() => this.remove(index)}>x</button>
+              </div>
+            )
+          ) }
+        </ReactCssTransitionGroup>
+        <button onClick={() => {
+            this.setState({ items: [...this.state.items, new Date().getTime()] })
+          }}>Add</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
