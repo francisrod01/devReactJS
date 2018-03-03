8d98aeceab142e63d58fb7a455c5c6cd80f4e4d4
Francis Rodrigues <francis.rod01@outlook.com>
2018-03-03 12:03:51 -0300

    [my-ip-app] Starting the first steps with our React Native app layout

--- projects/myip/App.js
+++ projects/myip/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
+
+import logo from './assets/logo.png'
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+		<View style={styles.body}>
+			<Image source={logo} />
+			<Text style={styles.ip}>IP</Text>
+			<Button title='Discover my IP' />
+		</View>
+		<View style={styles.footer}>
+			<Text style={styles.made}>
+				Made by Francis Rodrigues
+			</Text>
+		</View>
       </View>
-    );
+    )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2F2336',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+  },
+  body: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  ip: {
+    color: 'white',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  footer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  made: {
+    color: 'white',
+    textAlign: 'center',
+  },
+})
