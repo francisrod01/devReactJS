33ca37780cb6bde0ab756a0dca8fc7d15448121a
Francis Rodrigues <francis.rod01@outlook.com>
2018-03-05 18:47:26 -0300

    [calculator-app] Added TouchableOpacity to buttons and change display state

--- projects/calculator-app/App.js
+++ projects/calculator-app/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // Defining bindables.
+    this.handleOp = this.handleOp.bind(this)
+
+    // Defining states.
+    this.state = {
+      display: '',
+      result: '',
+    }
+  }
+  handleOp(op) {
+    this.setState({
+      display: this.state.display + op,
+    })
+  }
   render() {
     const col1Buttons = [
       ['7', '8', '9'],
@@ -20,27 +37,27 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
 
-        <Text style={styles.display}>Display</Text>
+        <Text style={styles.display}>{this.state.display}</Text>
 
-        <Text style={styles.result}>Result</Text>
+        <Text style={styles.result}>{this.state.result}</Text>
 
         <View style={styles.buttons}>
           <View style={styles.col1}>
             { col1Buttons.map((line, i) => (
               <View key={i} style={styles.line}>
                 { line.map((op, y) => (
-                  <View key={y} style={styles.btn}>
+                  <TouchableOpacity key={y} style={styles.btn} onPress={() => this.handleOp(op)}>
                     <Text style={styles.btnText}>{op}</Text>
-                  </View>
+                  </TouchableOpacity>
                 )) }
               </View>
             )) }
           </View>
           <View style={styles.col2}>
             { col2Buttons.map((op, i) => (
-              <View key={i} style={styles.btn}>
+              <TouchableOpacity key={i} style={styles.btn} onPress={() => this.handleOp(op)}>
                   <Text style={styles.btnText}>{op}</Text>
-              </View>
+              </TouchableOpacity>
             )) }
           </View>
         </View>
