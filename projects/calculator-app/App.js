9a4f20f928cc9271ef08c479b4cfe772049bd5f6
Francis Rodrigues <francis.rod01@outlook.com>
2018-03-05 12:39:40 -0300

    [calculator-app] Defined layout from our calculator

--- projects/calculator-app/App.js
+++ projects/calculator-app/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
+        <Text style={styles.display}>Display</Text>
+
+        <Text style={styles.result}>Result</Text>
+
+        <View style={styles.buttons}>
+          <View style={styles.col1}>
+            <Text>Col1</Text>
+          </View>
+          <View style={styles.col2}>
+            <Text>Col2</Text>
+          </View>
+        </View>
+
       </View>
-    );
+    )
   }
 }
 
@@ -17,7 +29,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-});
+  display: {
+    flex: 1,
+    paddingTop: 30,
+    paddingRight: 10,
+    backgroundColor: '#EFEFEF',
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  result: {
+    flex: 0.4,
+    paddingRight: 10,
+    paddingBottom: 10,
+    backgroundColor: '#EFEFEF',
+    fontSize: 20,
+    textAlign: 'right',
+  },
+  buttons: {
+    flex: 5,
+    flexDirection: 'row',
+  },
+  col1: {
+    flex: 3,
+    backgroundColor: 'grey',
+  },
+  col2: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+})
