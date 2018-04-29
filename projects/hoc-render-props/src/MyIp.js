e64b5787318e6478206df3256d906157e2be01ef
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-29 15:56:38 -0300

    [hoc-app] Convert MyIp Component to MyIp Stateless Component

--- projects/hoc-render-props/src/MyIp.js
+++ projects/hoc-render-props/src/MyIp.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import withHttp from './withHttp'
 
-class MyIp extends Component {
-  render() {
-    if (this.props.isLoading) {
-      return <p>Loading...</p>
-    }
-    return (
-      <p>My ip is: {this.props.data.origin}</p>
-    )
+const MyIp = props => {
+  if (props.isLoading && props.data) {
+    return <p>Loading...</p>
   }
+  return (
+    <p>My ip is: {props.data.origin}</p>
+  )
 }
 
 export default withHttp(MyIp)
