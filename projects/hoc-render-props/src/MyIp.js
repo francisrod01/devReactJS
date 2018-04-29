073a855e890f838071a5fa8d82838ef1971aca9e
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-29 15:22:32 -0300

    [hoc-app] Implementing MyIp component

--- projects/hoc-render-props/src/MyIp.js
+++ projects/hoc-render-props/src/MyIp.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class MyIp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: false,
+      origin: null
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    })
+    
+    axios
+      .get('http://httpbin.org/ip')
+      .then(res => {
+        const { origin } = res.data
+        if (origin) {
+          this.setState({
+            isLoading: false,
+            origin
+          })
+        }
+      })
+  }
   render() {
+    const { isLoading, origin } = this.state
+    if (isLoading) {
+      return <p>Loading...</p>
+    }
     return (
-      <p>My ip is:</p>
+      <p>My ip is: {origin}</p>
     )
   }
 }
