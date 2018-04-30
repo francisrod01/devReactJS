952cacd1daeff718d5f8f1956bc2933e4f54b466
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-29 22:26:17 -0300

    [context-api] Working with Prop Drilling

--- projects/context-api/README.md
+++ projects/context-api/README.md
@@ -10,6 +10,44 @@ Working with props using Context from React.
 
 In a typical React application, data is passed top-down (parent to child) via props, but this can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like this between components without having to explicitly pass a prop through every level of the tree.
 
+**Passing props as `Prop Drilling`:**
+
+```jsx
+import React, { Component } from 'react'
+
+const Title = props => (
+  // props.auth.user return a 'john.doe'
+  <h1 className="App-title">Welcome {props.auth.user}</h1>
+)
+
+const Header = props => (
+  <header className="App-header">
+    <Title auth={props.auth} /> {/* This work is very hard to mainteinance */}
+  </header>
+)
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      auth: {
+        user: 'john.doe',
+        isAuth: false
+      }
+    }
+  }
+  render() {
+    // The `this.state.auth` pass 'john.doe' by `auth` prop.
+    return (
+      <div className="App">
+        <Header auth={this.state.auth} />
+      </div>
+    )
+  }
+}
+```
+
 ## References ##
 
 - [ReactJS][2]
--- projects/context-api/src/App.js
+++ projects/context-api/src/App.js
@@ -3,6 +3,22 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+
+const Title = props => {
+  if (!props.auth.isAuth) {
+    return <h1>Please, log-in</h1>
+  }
+  return <h1 className="App-title">Welcome {props.auth.user}</h1>
+}
+
+const Header = props => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+
+    <Title auth={props.auth} />
+  </header>
+)
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -17,12 +33,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome {this.state.auth.user}</h1>
-        </header>
+        <Header auth={this.state.auth} />
+        
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={() => {
+            if (this.state.auth.isAuth) {
+              this.setState({
+                auth: {
+                  user: null, isAuth: false
+                }
+              })
+            }
+            else {
+              this.setState({
+                auth: {
+                  user: 'john.doe', isAuth: true
+                }
+              })
+            }
+          }}>Toggle auth</button>
         </p>
       </div>
     )
