5e015289ef864ce5fb3b2976ac6aecfce291dfd7
Francis Rodrigues <francis.rod01@outlook.com>
2018-05-01 01:20:41 -0300

    [bora-ajudar] Added firebase config using environment variables

---
+++ projects/bora-ajudar/src/base.js
@@ -0,0 +1,18 @@
+import rebase from 're-base';
+import firebase from 'firebase';
+
+const envs = process.env;
+
+const config = {
+  apiKey: envs.REACT_APP_FIREB_API_KEY,
+  authDomain: envs.REACT_APP_FIREB_AUTH_DOMAIN,
+  databaseURL: envs.REACT_APP_FIREB_DATABASE_URL,
+  projectId: envs.REACT_APP_FIREB_PROJECT_ID,
+  storageBucket: envs.REACT_APP_FIREB_STORAGE_BUCKET,
+  messagingSenderId: envs.REACT_APP_FIREB_MESSAGING_SENDER_ID
+}
+
+const app = firebase.initializeApp(config);
+const base = rebase.createClass(app.database());
+
+export default base;
