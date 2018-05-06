85b78e1a46de121c79577932d803b2dfb71ef7b8
Francis Rodrigues <francis.rod01@outlook.com>
2018-05-06 15:59:44 -0300

    [bora-ajudar] Added Error404 route using Switch in App component

--- projects/bora-ajudar/src/App.js
+++ projects/bora-ajudar/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
+  Switch,
   Route
 } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ import Admin from './Admin';
 import Login from './Login';
 import Contact from './Contact';
 import Footer from './Footer';
+import Error404 from './Error404';
 
 
 class App extends Component {
@@ -21,12 +23,15 @@ class App extends Component {
         <div>
           <Header />
 
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/campaigns' component={Campaigns} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/admin' component={Admin} />
-          <Route path='/login' component={Login} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/campaigns' component={Campaigns} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/admin' component={Admin} />
+            <Route path='/login' component={Login} />
+            <Route component={Error404} />
+          </Switch>
 
           <Footer />
         </div>
