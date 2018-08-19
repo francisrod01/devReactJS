59fbf3b2ae02cd9ee803a3cfcbf3e73aa8e9837d
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-19 20:10:06 -0300

    [dev-running] Implement Login class with some events
    Some events: handleChange, onSignIn, onclick

--- projects/dev-running/src/screens/Login.js
+++ projects/dev-running/src/screens/Login.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const Login = props => <h1>Login</h1>
+class Login extends Component {
+  state = {
+    form: {
+      email: '',
+      passwd: '',
+    }
+  }
+  handleChange = fieldName => event => {
+    const form = {
+      ...this.state.form,
+    }
+    form[fieldName] = event.target.value;
+    this.setState({ form });
+  }
+  onSignIn = () => {
+    console.log('on sign in...', this.state.form);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <input
+          type='text'
+          value={this.state.form.email}
+          onChange={this.handleChange('email')} />
+
+        <input
+          type='text'
+          value={this.state.form.passwd}
+          onChange={this.handleChange('passwd')} />
+
+        <button onClick={this.onSignIn}>Sign In</button>
+
+        <p>{ JSON.stringify(this.state) }</p>
+      </div>
+    );
+  }
+}
 
 export default Login;
