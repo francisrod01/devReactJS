4afb999cd5a546851f8609feb0a9b16c08bc6126
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-19 19:45:44 -0300

    [dev-running] Implement admin sub routes

--- projects/dev-running/src/screens/Admin/index.js
+++ projects/dev-running/src/screens/Admin/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 
-const Admin = props => <h1>Admin</h1>
+const AdminHome = props => <h2>Admin Home</h2>
+const AdminUsers = props => <h2>Admin Users</h2>
+
+const Admin = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Admin</h1>
+
+      <p>
+        <Link to='/admin'>Home</Link>
+        <Link to='/admin/users'>Users</Link>
+      </p>
+
+      <div>
+        <Route exact path={`${props.match.path}/`} component={AdminHome} />
+        <Route path={`${props.match.path}/users`} component={AdminUsers} />
+      </div>
+    </div>
+  );
+}
 
 export default Admin;
