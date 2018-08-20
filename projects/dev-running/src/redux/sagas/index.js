8d7a5adacc814b31a9627855e89405b12702053f
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-20 04:44:56 -0300

    [dev-running] Some implements on sagas
    - imported takeLatest and actionCreators
    - called to signin_request

--- projects/dev-running/src/redux/sagas/index.js
+++ projects/dev-running/src/redux/sagas/index.js
@@ -1,3 +1,13 @@
+import { takeLatest } from 'redux-saga/effects';
+
+import { Types } from '../actionCreators';
+
+
+function* signIn(action) {
+  console.log('saga signIn: ', action);
+}
+
 export default function* rootSaga() {
   console.log('root saga');
+  yield takeLatest(Types.SIGNIN_REQUEST, signIn);
 }
