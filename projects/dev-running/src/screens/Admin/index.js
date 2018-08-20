2659c50623fa0664cd1211577fb87eba59853020
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-20 06:02:49 -0300

    [dev-running] Some implementations on Admin screen
    - Check is auth, role and redirect
    - Added mapStateToProps and connect it

--- projects/dev-running/src/screens/Admin/index.js
+++ projects/dev-running/src/screens/Admin/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 const AdminHome = props => <h2>Admin Home</h2>
 const AdminUsers = props => <h2>Admin Users</h2>
 
 const Admin = props => {
-  console.log(props);
+  if (!props.auth.isAuth) {
+    return <Redirect to='/login' />
+  }
+  if (props.auth.user.role !== 'admin') {
+    return <Redirect to='/restrict' />
+  }
   return (
     <div>
       <h1>Admin</h1>
 
       <p>
+        {JSON.stringify(props.auth)}
         <Link to='/admin'>Home</Link>
         <Link to='/admin/users'>Users</Link>
       </p>
@@ -24,4 +31,12 @@ const Admin = props => {
   );
 }
 
-export default Admin;
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
+
+
+export default connect(mapStateToProps)(Admin);
