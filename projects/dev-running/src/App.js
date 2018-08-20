7d4128726fd07309cfa53cabf78e47fe4e495dff
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-20 05:20:57 -0300

    [dev-running] Refactor call to login using sagas

--- projects/dev-running/src/App.js
+++ projects/dev-running/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
 import { Provider } from 'react-redux';
 import {
   Route,
@@ -19,26 +17,6 @@ import Login from './screens/Login';
 
 
 class App extends Component {
-  /*async componentDidMount() {
-    let token = localStorage.getItem('token');
-    if (!token) {
-      const login = await axios.post('http://localhost:3001/users/login', {
-        email: 'tuliofaria@devpleno.com',
-        passwd: 'abc123'
-      });
-      token = login.data.token;
-      localStorage.setItem('token', token);
-    }
-    const decoded = jwtDecode(token);
-    console.log(decoded);
-
-    const user = await axios.get('http://localhost:3001/users/me', {
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    });
-    console.log('=== user me: ', user);
-  }*/
   render() {
     return (
       <Provider store={store}>
--- projects/dev-running/src/redux/sagas/index.js
+++ projects/dev-running/src/redux/sagas/index.js
@@ -1,13 +1,32 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, all, put } from 'redux-saga/effects';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-import { Types } from '../actionCreators';
+import ActionCreators, { Types } from '../actionCreators';
 
 
 function* signIn(action) {
-  console.log('saga signIn: ', action);
+  let token = localStorage.getItem('token');
+  const login = yield axios.post('http://localhost:3001/users/login', {
+    email: action.email,
+    passwd: action.passwd,
+  });
+  if (login.data.token) {
+    token = login.data.token;
+    localStorage.setItem('token', token);
+
+    const userDecoded = jwtDecode(token);
+    localStorage.setItem('user', userDecoded);
+
+    yield put(ActionCreators.signinSuccess(userDecoded));
+  }
+  else {
+    yield put(ActionCreators.signinFailure(login.data.message));
+  }
 }
 
 export default function* rootSaga() {
-  console.log('root saga');
-  yield takeLatest(Types.SIGNIN_REQUEST, signIn);
+  yield all([
+    takeLatest(Types.SIGNIN_REQUEST, signIn),
+  ]);
 }
