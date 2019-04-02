a56d7d67400b2619a39dfcd3f34acd774937104b
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-02 17:55:34 -0300

    [dev-running] Added my-account restrict screen

---
+++ projects/dev-running/src/screens/Restrict/MyAccount.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { Button } from 'semantic-ui-react';
+import timezones from 'moment-timezone/data/meta/latest.json';
+
+import ActionCreators from '../../redux/actionCreators';
+
+
+class MyAccount extends Component {
+  state = {
+    unit: '',
+    timezone: ''
+  }
+  componentDidMount() {
+    const { auth } = this.props;
+
+    this.setState({
+      unit: auth.user.unit,
+      timezone: auth.user.timezone
+    });
+  }
+  handleChange = fieldName => event => {
+    this.setState({
+      [fieldName]: event.target.value
+    });
+  }
+  handleSave = () => {
+    const { unit, timezone } = this.state;
+
+    this.props.save({
+      id: this.props.auth.user.id,
+      unit,
+      timezone
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Account</h1>
+
+        <select
+          value={this.state.unit}
+          onChange={this.handleChange('unit')}>
+          <option value='metric'>Metric (km)</option>
+          <option value='imperial'>Imperial (mi)</option>
+        </select>
+
+        <select
+          value={this.state.timezone}
+          onChange={this.handleChange('timezone')}>
+
+          {Object.keys(timezones.zones).map(tz => (
+            <option key={ tz } value={ tz }>{ tz }</option>
+          ))}
+        </select>
+
+        <Button onClick={this.handleSave}>Save</Button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    save: (user) => dispatch(ActionCreators.updateProfileRequest(user)),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyAccount);
